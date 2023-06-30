d0d859d93cc4379f38b57cf9ae3085777dd4f677
3anza <salmaaldubouni@gmail.com>
2023-06-30 15:13:36 +0200

    path correction

--- js/rewards.js
+++ js/rewards.js
@@ -4,10 +4,10 @@ class Reward {
             this.left = Math.floor(Math.random() * 900 + 900)
             this.top = Math.floor(Math.random() * 500 + 100)
             this.width = 100
-            this.height = 70
+            this.height = 50
             this.element = document.createElement('img')
     
-            this.element.src = '../images/lamp4.png'
+            this.element.src = './images/lamp4.png'
             this.element.style.position = 'absolute'
             this.element.style.width = `${this.width}px`
             this.element.style.height = `${this.height}px`
